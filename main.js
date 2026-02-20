183f07f8a83265ed6a7b1cf8a19e280195bf71c8
Midushekaa <abe.midu@gmail.com>
2026-02-20 11:52:01 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+// ScrollReveal animations
+ScrollReveal().reveal('#home h1', {
+  delay: 300,
+  distance: '60px',
+  origin: 'bottom',
+  duration: 1200
+});
+
+ScrollReveal().reveal('#home p', {
+  delay: 500,
+  distance: '40px',
+  origin: 'bottom',
+  duration: 1000
+});
+
+ScrollReveal().reveal('#home .btn-custom', {
+  delay: 700,
+  distance: '30px',
+  origin: 'bottom',
+  duration: 800
+});
+
+ScrollReveal().reveal('.card', {
+  interval: 200,
+  distance: '50px',
+  origin: 'bottom',
+  duration: 1000
+});
+
+ScrollReveal().reveal('form', {
+  delay: 400,
+  distance: '50px',
+  origin: 'bottom',
+  duration: 1000
+});
